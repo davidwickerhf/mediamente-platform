6d1c55e53f7b11f42dd7ff3040a35c4aa10ff64a
David Wicker <davidwickerhf@gmail.com>
2022-07-09 14:41:14 +0200

    Macchine Page initial commit and styling

--- views/macchine/index.php
+++ views/macchine/index.php
@@ -1,1 +1,69 @@
-TEST
+<div class="macchine-calendario">
+    <h1 class="page-title">
+        Macchine Aziendali
+    </h1>
+
+    <!-- Macchine / Calendario  -->
+    <div>
+        <!-- Banner -->
+        <div class="banner">
+            <!-- Section Prossime Prenotazioni -->
+            <div class="banner-section banner-prossime">
+                <div class="section-head">
+                    <div class="title-dropdown">
+                        <div class="title-dropdown-button">
+                            <h2 class="section-title">
+                                Prossime prenotazioni
+                            </h2>
+                            <i class="fa fa-solid fa-angle-down icon-white"></i>
+                        </div>
+                        <div class="dropdown-content">
+                            <a href="#" class="dropdown-item">
+                                Prossime prenotazioni
+                            </a>
+                            <a href="#" class="dropdown-item">
+                                Prenotazioni in corso
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="divider"></div>
+
+            <!-- Section Statistiche  -->
+            <div class="banner-section banner-statistiche">
+                <div class="section-head">
+                    <h2 class="section-title">Prenotazioni</h2>
+                    <div class="button-dropdown">
+                        <div class="button-dropdown-button">
+                            <h2 class="section-title">
+                                Mesi
+                            </h2>
+                            <i class="fa fa-solid fa-angle-down icon-white"></i>
+                        </div>
+                        <div class="dropdown-content">
+                            <a href="#" class="dropdown-item">
+                                Mesi
+                            </a>
+                            <a href="#" class="dropdown-item">
+                                Anni
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="divider"></div>
+
+            <!-- Section Disponibilita -->
+            <div class="banner-section banner-disponibilita">
+                <div class="section-head">
+                    <h2 class="section-title">Disponibilita'</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</div>
